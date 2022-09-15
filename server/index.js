a799dfc559451871b90490f9541883adad6e7276
Kathryn Gao <kathryn.kuroko@gmail.com>
2022-09-14 21:02:29 -0700

    added routes

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 require("dotenv").config();
 const express = require('express');
 const morgan = require('morgan');
+const products = require('./router.js')
 const app = express();
 const db = require('./db/mongoDB.js')
 
 app.use(express.json());
 app.use(morgan('dev'));
+
+app.use('/products', products)
 
 app.listen(process.env.PORT);
 
---
+++ server/router.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.send('product end point')
+})
+
+router.get('/:product_id', (req, res) => {
+  res.send('product id end point')
+})
+
+router.get('/:product_id/styles', (req, res) => {
+  res.send('style end point')
+})
+
+router.get('/:product_id/related', (req, res) => {
+  res.send('related end point')
+})
+
+
+
+module.exports = router;
+
