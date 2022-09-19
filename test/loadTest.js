8feca58f8014be226258ce1d8bca4c8e6781344d
Kathryn Gao <kathryn.kuroko@gmail.com>
2022-09-19 11:17:56 -0700

    shorter run time for k6 tests

--- test/loadTest.js
+++ test/loadTest.js
@@ -1,14 +1,13 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
-import endpoints from "./endpoints.js"
+import { sleep, check } from 'k6';
 
 export const options = {
   insecureSkipTLSVerify: true,
   noConnectionReuse: false,
   stages: [
-    { duration: '5m', target: 100 },
-    { duration: '10m', target: 100 },
-    { duration: '5m', target: 0 },
+    { duration: '1m', target: 100 },
+    { duration: '2m', target: 100 },
+    { duration: '1m', target: 0 },
   ],
   thresholds: {
     http_req_duration: ['p(99)<50']
@@ -17,14 +16,14 @@ export const options = {
 
 
 export default function () {
-  const getRandomInt(min, max) {
-    return Math.random() * (max - min) + min;
+  const getRandomInt = (min, max) => {
+    return Math.floor(Math.random() * (max - min) + min);
   }
 
-  const res = http.get(`http://localhost:8080/products?page=${getRandomInt(100000, 100000)}&count=5`);
-  // const res = http.get(`http://localhost:8080/products/${getRandomInt(100000, 1000000)}`);
-  // const res = http.get(`http://localhost:8080/products/${getRandomInt(100000, 1000000)}/styles`);
-  // const res = http.get(`http://localhost:8080/products/${getRandomInt(100000, 1000000)}/related`);
+  // const res = http.get(`http://localhost:8080/products?page=${getRandomInt(1e4, 1e5)}&count=5`);
+  // const res = http.get(`http://localhost:8080/products/${getRandomInt(1e5, 1e6)}`);
+  const res = http.get(`http://localhost:8080/products/${getRandomInt(1e5, 1e6)}/styles`);
+  // const res = http.get(`http://localhost:8080/products/${getRandomInt(1e5, 1e6)}/related`);related`);
   check(res, { 'status was 200': (res) => res.status == 200 });
 
   sleep(1);
--- test/stressTest.js
+++ test/stressTest.js
@@ -1,19 +1,19 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
+import { sleep, check} from 'k6';
 
 export const options = {
   insecureSkipTLSVerify: true,
   noConnectionReuse: false,
   stages: [
+    { duration: '1m', target: 1 },
     { duration: '2m', target: 1 },
-    { duration: '5m', target: 1 },
+    { duration: '1m', target: 10 },
     { duration: '2m', target: 10 },
-    { duration: '5m', target: 10 },
+    { duration: '1m', target: 100 },
     { duration: '2m', target: 100 },
-    { duration: '5m', target: 100 },
+    { duration: '1m', target: 1000 },
     { duration: '2m', target: 1000 },
-    { duration: '5m', target: 1000 },
-    { duration: '10m', target: 0 },
+    { duration: '4m', target: 0 },
   ],
   thresholds: {
     http_req_duration: ['p(99)<50']
@@ -21,14 +21,14 @@ export const options = {
 };
 
 export default function () {
-  const getRandomInt(min, max) {
+  const getRandomInt = (min, max) => {
     return Math.random() * (max - min) + min;
   }
 
-  const res = http.get(`http://localhost:8080/products?page=${getRandomInt(100000, 100000)}&count=5`);
-  // const res = http.get(`http://localhost:8080/products/${getRandomInt(100000, 1000000)}`);
-  // const res = http.get(`http://localhost:8080/products/${getRandomInt(100000, 1000000)}/styles`);
-  // const res = http.get(`http://localhost:8080/products/${getRandomInt(100000, 1000000)}/related`);
+  // const res = http.get(`http://localhost:8080/products?page=${getRandomInt(1e4, 1e5)}&count=5`);
+  const res = http.get(`http://localhost:8080/products/${getRandomInt(1e5, 1e6)}`);
+  // const res = http.get(`http://localhost:8080/products/${getRandomInt(1e5, 1e6)}/styles`);
+  // const res = http.get(`http://localhost:8080/products/${getRandomInt(1e5, 1e6)}/related`);
   check(res, { 'status was 200': (res) => res.status == 200 });
 
   sleep(1);
