9cea8ef6e914e0d3f2a4bd656e447004b2fc974f
Kathryn Gao <kathryn.kuroko@gmail.com>
2022-09-20 11:49:55 -0700

    loader

--- server/routers/router.js
+++ server/routers/router.js
@@ -10,7 +10,7 @@ router.get('/:product_id/styles', controller.getStyles)
 
 router.get('/:product_id/related', controller.getRelated);
 
-router.get('/loaderio-60dd52b0ab1a1dc903078d5f873be0f1', (req, res) => res.send("loaderio-60dd52b0ab1a1dc903078d5f873be0f1"));
+router.get('/loaderio-60dd52b0ab1a1dc903078d5f873be0f1/', (req, res) => res.send("loaderio-60dd52b0ab1a1dc903078d5f873be0f1"));
 
 module.exports = router;
 
