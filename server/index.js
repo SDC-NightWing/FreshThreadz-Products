ece16a42ba6740ca3d6c2cb65501bd410e5b76a1
Kathryn Gao <kathryn.kuroko@gmail.com>
2022-09-21 12:30:51 -0700

    loader

--- server/index.js
+++ server/index.js
@@ -13,6 +13,8 @@ app.get('/loaderio-daadd206ffb8d48319ab4cd1c68574a3/', (req, res) => res.send("l
 
 app.get('/loaderio-60dd52b0ab1a1dc903078d5f873be0f1/', (req, res) => res.send("loaderio-60dd52b0ab1a1dc903078d5f873be0f1"));
 
+app.get('/loaderio-9e620486d1d4c95489a43916177cc3a0/', (req, res) => res.send("loaderio-9e620486d1d4c95489a43916177cc3a0"));
+
 app.use('/products', products)
 
 app.listen(process.env.PORT);
